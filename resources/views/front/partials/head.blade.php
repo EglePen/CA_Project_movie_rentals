6dd73478e050333071d0f9eaf6674c41786ec617
EglePen <112707050+EglePen@users.noreply.github.com>
2023-04-13 23:12:05 +0300

    added font awesome kit

--- resources/views/front/partials/head.blade.php
+++ resources/views/front/partials/head.blade.php
@@ -5,3 +5,4 @@
 <link rel="stylesheet" href="{{ asset('css/front/form.css') }}">
 <link rel="stylesheet" href="{{ asset('css/front/navigation.css') }}">
 <link rel="stylesheet" href="{{ asset('css/front/buttons.css') }}">
+<script src="https://kit.fontawesome.com/00f36d8308.js" crossorigin="anonymous"></script>
