e85d2265ca95bb486b3c3da35e3f92d17ea8cf96
EglePen <112707050+EglePen@users.noreply.github.com>
2023-04-13 23:14:00 +0300

    added search form

--- resources/views/front/partials/navigation.blade.php
+++ resources/views/front/partials/navigation.blade.php
@@ -7,14 +7,18 @@
         <li class="nav-item">
             <a class="nav-link" href="/lang/lt">[LT]</a>
         </li>
-        <li class='dropdown'>
-            <i class="fa fa-caret-down" aria-hidden="true"></i>
+        <li class="nav-item">
             <a href="{{route('front.movies.index')}}">{{Str::ucfirst(trans('app.movies'))}}</a>
-            <ul class='dropdown-list'>
-                <li>
-                    <a href="{{route('front.movies.index')}}">{{Str::ucfirst(trans('app.all'))}}</a>
-                </li>
-            </ul>
         </li>
     </ul>
+    <div>
+        <form action="{{route('front.search')}}" method="GET" role="search" class="flex-end">
+            <div class="input-group front-input">
+                <input type="search" name="search" value="" placeholder="@lang('app.search_for_a_movie')" class="form-control form-control-lg"/>
+                <button class="btn bg-lg btn" type="submit">
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </button>
+            </div>
+        </form>
+    </div>
 </nav>
