3638bbc5c202585f5d885aa2518a0333e3cec539
EglePen <112707050+EglePen@users.noreply.github.com>
2023-04-18 19:16:16 +0300

    modified show blade

--- resources/views/front/movies/show.blade.php
+++ resources/views/front/movies/show.blade.php
@@ -1,55 +1,63 @@
 @extends('front.layouts.document')
 
 @section('content')
+
+
 <section>
-    <header>
+
+    <div class="movie-title">
         <h1>
             {{$model->title}}
         </h1>
-    </header>
-
-    <div class="single-game-card-wrapper">
-        <article class="game-card">
-            <img src="{{asset('storage/images/' . ($model->image ?? ''))}}">
-            <div class="game-card-body">
-                <div class="game-card-details">
+    </div>
+        <article class="single-movie-card-wrapper">
+            <div class ="single-movie-img" >
+                <img src="{{asset('storage/images/' . ($model->image ?? ''))}}">
+            </div>
+            <div class="single-movie-card-body">
+                <div class="">
                     <div>
-                        <span>@langTitle('app.release_date')</span>
+                        <span>@langTitle('app.release_date'):</span>
                         <span>{{ ($model->release_date ?? '') }}</span>
                     </div>
                     <div>
-                        <span>@langTitle('app.runtime')</span>
+                        <span>@langTitle('app.runtime'):</span>
                         <span>{{ ($model->runtime ?? '')}} @lang('app.min')</span>
                     </div>
                     <div>
-                        <span>@langTitle('app.rating')</span>
+                        <span>@langTitle('app.rating'):</span>
                         <span>{{ ($model->rating ?? '') }}</span>
                     </div>
                     <div>
-                        <span>@langTitle('app.genres')</span>
+                        <span>@langTitle('app.genres'):</span>
                         <span>{{ ($model?->genres->pluck('name')->implode(', ')) }}</span>
                     </div>
                     <div>
-                        <span>@langTitle('app.languages')</span>
+                        <span>@langTitle('app.actors'):</span>
+                        <span>{{ ($model?->actors->pluck('fullName')->implode(', ')) }}</span>
+                    </div>
+                    <div>
+                        <span>@langTitle('app.languages'):</span>
                         <span>{{ ($model?->languages->pluck('name')->implode(', ')) }}</span>
                     </div>
                     <div>
-                        <span>@langTitle('app.countries')</span>
+                        <span>@langTitle('app.countries'):</span>
                         <span>{{ ($model?->countries->pluck('name')->implode(', ')) }}</span>
                     </div>
                 </div>
-                <div class="game-card-description">
+                <div class="description">
                     {{ ($model->description ?? '') }}
                 </div>
             </div>
         </article>
+
         <aside>
-            <div>
+            <div class="single-movie-card-images">
                 @foreach($model->images as $image)
                     <img src="{{asset('storage/images/' . ($image->name ?? ''))}}">
                 @endforeach
             </div>
         </aside>
-    </div>
+
 </section>
 @endsection
